96cc989a9f9d3686af2f74848353d966aac7964f
Esa <esa-pitkanen@hotmail.com>
2024-10-23 19:12:12 +0300

    Create a function to reverse the string
    Created a loop that iterates over the length of the given string. After each loop, a character was added to the variable from the negative index number.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let wordReversed = ""
+    let lastChar = -1
+    for (;wordReversed.length < string.length;) {
+        wordReversed += string.at(lastChar);
+        lastChar--
+    }
+    return wordReversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
