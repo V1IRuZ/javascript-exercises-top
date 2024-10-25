5491d8a2d1f70fd11d3238355b7dc04b68027c15
Esa <esa-pitkanen@hotmail.com>
2024-10-25 18:39:33 +0300

    Remove the given arguments from the array
    A function was created that adds all array items to a new array that do not appear in the arguments. So, in practice, it removes the desired items from the new array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(list, ...Args) {
+    let newList = [];
+    const sortedArgs = Args.sort();
+    for (i = 0; i < list.length; i++) {
+        if (sortedArgs.includes(list[i]) == false) {
+            newList.push(list[i]);
+        }
+    }
+    return newList;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
