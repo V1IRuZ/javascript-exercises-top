3cdf0dcb6fbea0d56effafb4539bb9528c133e03
Esa <esa-pitkanen@hotmail.com>
2024-10-26 11:37:53 +0300

    Calculate the sum of the integers
    The function calculates the sum of integers between two values. If the function is not given a positive integer, it returns an error.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(valueOne, valueTwo) {
+    if (!Number.isInteger(valueOne) || !Number.isInteger(valueTwo)) {
+        return "ERROR";
+    } else if (valueOne < 0 || valueTwo < 0) {
+        return "ERROR";
+    } else {
+        let numbers = [valueOne, valueTwo];
+        numbers.sort(function(a, b){return a - b});
+        let sum = 0;
+        
+        for (i = numbers[0]; i <= numbers[1]; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
