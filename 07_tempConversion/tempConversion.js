0a65e3748c934e84532b0269cd6ddf6ac2fd6ade
Esa <esa-pitkanen@hotmail.com>
2024-10-26 14:29:43 +0300

    Convert Fahrenheit to Celsius and vice versa
    Created two functions that calculate Fahrenheit and Celsius.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheit) {
+  let toCelsius = (fahrenheit - 32) * 5/9;
+  return parseFloat(toCelsius.toFixed(1));
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let toFahrenheit = (celsius * 9 / 5 + 32);
+  return parseFloat(toFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
