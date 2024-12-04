d40ebb16c7f4ed02cbfa018983aa1dc1bf94459b
Esa <esa-pitkanen@hotmail.com>
2024-12-04 18:12:35 +0200

    Determine if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const regExp = /[^a-zA-Z0-9]/g;
+
+    const testString = string.split(regExp).join("").toLowerCase();
+  
+    const reversedString = testString
+    .split("")
+    .reverse()
+    .join("");
+
+    if (testString === reversedString) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
