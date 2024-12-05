124ce6e9b04048d616978fb86c9de1b00f9dfeba
Esa <esa-pitkanen@hotmail.com>
2024-12-05 15:48:20 +0200

    Calculate the next number in the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) {
+    return "OOPS"
+  }
 
+    let current = 0;
+    let previous = 1;
+    let result = 0
+    
+    for (let i = 0; i < num; i++) {
+     
+      result = previous + current;
+      previous = current;
+      current = result;
+    }
+    return result
 };
 
 // Do not edit below this line
