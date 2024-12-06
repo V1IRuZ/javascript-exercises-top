1be5f926a660d9a7053b615707a38dd81e9f5612
Esa <esa-pitkanen@hotmail.com>
2024-12-06 16:29:31 +0200

    Find the oldest person from array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.map((person) => {
+         if (!person.yearOfDeath) {
+           person.yearOfDeath = (new Date()).getFullYear();
+           return person
+         } else {
+           return person
+         }
+      }).sort((a, b) => {
+           const lastPerson = a.yearOfDeath - a.yearOfBirth;
+           const nextPerson = b.yearOfDeath - b.yearOfBirth;
+           return lastPerson > nextPerson ? -1 : 1;
+       })
+    return oldest[0];
 };
 
 // Do not edit below this line
